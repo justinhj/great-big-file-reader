3cb864a9a3bd09aff6047cca11927a9f14040e18
Justin Heyes-Jones <justinhj@gmail.com>
2023-07-17 19:37:43 -0700

    latest

--- test/test.js
+++ test/test.js
@@ -1,22 +1,57 @@
 import test from 'tape';
+import { promises as fs } from 'fs';
+import { MMapping } from '../lib/index.js';  
 
-test('timing test', function (t) {
-    t.plan(2);
+// test reading a large file
+test('large file read', async function (t) {
+  const offset = 5451234524n; 
+  const filename = '/Users/justin.heyes-jones/projects/lantern/build/sparkey100million.spl';
 
-    t.equal(typeof Date.now, 'function');
-    var start = Date.now();
+  const fh = await fs.open(filename, 'r');
+  
+  let m1 = new MMapping(filename, fh.fd);
+  console.log(m1);
 
-    setTimeout(function () {
-        t.closeTo(Date.now() - start, 100);
-    }, 100, 3);
+  let b1 = m1.getBuffer(offset + 2n, 36);
+  console.log(b1);
+  console.log(b1.toString());
+
+  let b2 = m1.getBuffer(0n, 4);
+  console.log(b2);
+
+  t.equal(Buffer.from([0x49, 0xb3, 0x9c, 0x95]).reverse().compare(b2),0);
+
+  m1.unmap();
+
+  await fh.close();
+  t.ok(true);
+  t.end();
 });
 
-async function someAsyncThing() {
-  return Promise.resolve(true);
+
+function createTestFile() {
+  const filename = 'testfile.dat';
+  const count = 1000000000;
+  console.log('Checking for test file');
+  if (!fs.existsSync(filename)) {
+    console.log('Creating test file');
+    const fd = fs.openSync(filename, 'w');
+    for(let i=0n; i<count; i++) {
+      const buffer = Buffer.alloc(8);
+      buffer.writeBigInt64LE(i);
+      fs.writeSync(fd, buffer, 0, buffer.length);
+    }
+    if(count % 1000000 === 0) {
+      console.log('.');
+    }
+    fs.closeSync(fd);
+  }
+  console.log('Done.');
 }
 
-test('test using promises', async function (t) {
-    const result = await someAsyncThing();
-    t.equal(result, true);
-    t.end();
+// createTestFile();
+
+test('addition test', function (t) {
+  t.equal(2, 1+1);
+  t.end();
 });
