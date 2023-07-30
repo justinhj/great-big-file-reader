5f601556a3fdb3fa002e8b2c290fe8a69dbf347d
Justin Heyes-Jones <justinhj@gmail.com>
2023-07-29 22:50:04 -0700

    set minimum node version to 17

--- test/test.js
+++ test/test.js
@@ -34,69 +34,62 @@ test.createStream()
 
 BigInt.prototype.toJSON = function() { return this.toString() }
 
-// test('mapping with two buffers happy path', async function(t) {
-//   // mmap the file
-//   console.log('mmap');
-//   let mmapping = new MMapping(testFilePath, fileHandle.fd);
+test('mapping with two buffers happy path', async function(t) {
+  // mmap the file
+  let mmapping = new MMapping(testFilePath, fileHandle.fd);
 
-//   let buffer1 = null;
-//   // open a buffer at the first 1kb
-//   console.log(`getbuffer 1 ${JSON.stringify(mmapping)}`);
-//   try {
-//     buffer1 = mmapping.getBuffer(0n, 1024);
-//     let first = buffer1.readBigUInt64LE(0);
-//     t.equal(first, 0n);
-//     let last = buffer1.readBigUint64LE(1024 - 8);
-//     t.equal(last, 127n);
-//   } catch(e) {
-//     console.log(e.message);
-//   }
+  let buffer1 = null;
+  // open a buffer at the first 1kb
+  buffer1 = mmapping.getBuffer(0n, 1024);
+  let first = buffer1.readBigUInt64LE(0);
+  t.equal(first, 0n);
+  let last = buffer1.readBigUint64LE(1024 - 8);
+  t.equal(last, 127n);
 
-//   // open a second buffer in the last 1kb of the file
-//   console.log('getbuffer 2');
-//   let buffer2 = mmapping.getBuffer(3072n, 1024);
-//   let first2 = buffer2.readBigUInt64LE(0);
-//   t.equal(first2, 384n);
-//   let last2 = buffer2.readBigUint64LE(1024 - 8);
-//   t.equal(last2, 511n);
+  // open a second buffer in the last 1kb of the file
+  let buffer2 = mmapping.getBuffer(3072n, 1024);
+  let first2 = buffer2.readBigUInt64LE(0);
+  t.equal(first2, 384n);
+  let last2 = buffer2.readBigUint64LE(1024 - 8);
+  t.equal(last2, 511n);
 
-//   mmapping.unmap();
-//   t.end();
-// });
+  mmapping.unmap();
+  t.end();
+});
 
-// test('unhappy paths', async function(t) {
-//   // mmap the file
-//   let mmapping = new MMapping(testFilePath, fileHandle.fd);
+test('unhappy paths', async function(t) {
+  // mmap the file
+  let mmapping = new MMapping(testFilePath, fileHandle.fd);
 
-//   t.plan(6);
+  t.plan(6);
 
-//   t.throws(function () {
-//     mmapping.getBuffer(-1n, 1024);
-//   }, /offset should not be negative/);
+  t.throws(function () {
+    mmapping.getBuffer(-1n, 1024);
+  }, /offset should not be negative/);
 
-//   t.throws(function () {
-//     mmapping.getBuffer(0n, -1024);
-//   }, /length should be greater than zero/);
+  t.throws(function () {
+    mmapping.getBuffer(0n, -1024);
+  }, /length should be greater than zero/);
 
-//   t.throws(function () {
-//     mmapping.getBuffer(4096n, 1024);
-//   }, /offset must be within the file/);
+  t.throws(function () {
+    mmapping.getBuffer(4096n, 1024);
+  }, /offset must be within the file/);
 
-//   t.throws(function () {
-//     mmapping.getBuffer(3072n, 2024);
-//   }, /offset plus length must be within the file/);
+  t.throws(function () {
+    mmapping.getBuffer(3072n, 2024);
+  }, /offset plus length must be within the file/);
 
-//   t.throws(function () {
-//     mmapping.getBuffer(3072n, BigInt(buffer.constants.MAX_LENGTH) + 1n);
-//   }, /length 4294967297 exceeds the buffer\.constants\.MAX_LENGTH \(4294967296\)/);
+  t.throws(function () {
+    mmapping.getBuffer(3072n, BigInt(buffer.constants.MAX_LENGTH) + 1n);
+  }, /length 4294967297 exceeds the buffer\.constants\.MAX_LENGTH \(4294967296\)/);
 
-//   t.throws(function () {
-//     mmapping.getBuffer(0n, buffer.constants.MAX_LENGTH + 1);
-//   }, /length 4294967297 exceeds the buffer\.constants\.MAX_LENGTH \(4294967296\)/);
+  t.throws(function () {
+    mmapping.getBuffer(0n, buffer.constants.MAX_LENGTH + 1);
+  }, /length 4294967297 exceeds the buffer\.constants\.MAX_LENGTH \(4294967296\)/);
 
-//   mmapping.unmap();
-//   t.end();
-// });
+  mmapping.unmap();
+  t.end();
+});
 
 test('mapping the whole file', async function(t) {
   // mmap the file
